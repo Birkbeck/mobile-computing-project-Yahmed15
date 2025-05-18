7f28e5712b7abecc21dff1e8355aa33dd394d4c1
yousuf <y_ahmed15@icloud.com>
2025-05-18 23:32:30 +0100

    Initial commit - Culinary Companion Project files

---
+++ app/src/main/java/com/example/culinarycompanion/EditRecipeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.culinarycompanion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EditRecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
