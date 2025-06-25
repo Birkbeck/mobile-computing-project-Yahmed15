319d61ae09a102f016af95bfe7a94de61c521957
yousuf <y_ahmed15@icloud.com>
2025-06-25 20:05:54 +0100

    updated but doesnt build

--- build.gradle.kts
+++
@@ -1,8 +0,0 @@
-plugins {
-    id("com.android.application") version "8.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.9.23" apply false
-
-    // Navigation Safe-Args
-    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
-}
